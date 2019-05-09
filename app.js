bb03d1c8d1ff5a62ed8b90ba68f2e2dbc0b3b4be
Thanh Nguyen <Thanh.ngu94@gmail.com>
2019-05-09 14:26:49 +0200

    backend auth first push

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 const session      = require('express-session');
 const passport     = require('passport');
 
+
+const app = express();
 // session are used for login puroposes
+
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -22,21 +25,14 @@ app.use(
   })
 );
 
-
-
-
-
+// Add middleware passport 
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //////////////////////////////////////////
 ///////////GENERATOR OUTPUT///////////////
 //////////////////////////////////////////
-
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
-const app = express();
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -44,29 +40,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
 const index = require('./routes/index');
+const authRoutes   = require("./auth/auth_routes")
 app.use('/', index);
+app.use('/auth', authRoutes)
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/home", (req, res, next) => {
-  res.render("index");
+router.get("/", (req, res, next) => {
+  res.send("api server launched and ready to rock")
 });
 
 module.exports = router;
