d2dc3b33d2f09a8711b5bc2ec11c93c17c18a137
Susan Almassi <susan.z.almassi@gmail.com>
2019-05-15 23:50:43 +0200

    trying again to fix bug

--- app.js
+++ app.js
@@ -40,6 +40,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Cors middleware to connect both Front to back
 app.use(
   cors({
@@ -47,11 +56,7 @@ app.use(
     origin: [process.env.REACT_DOMAIN]
   })
 );
-app.all("/", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 //////////////////////////////////////////
 ///////////GENERATOR OUTPUT///////////////
 //////////////////////////////////////////
