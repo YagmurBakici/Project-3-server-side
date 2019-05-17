da24fed3fb595e09969c61fbd92b4be6a55a9450
sunwucode <wukongkhan8@gmail.com>
2019-05-17 15:18:20 +0200

    userRoute.js modified

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,39 @@
-const express = require("express")
-const router = new express.Router()
-const userModel = require("../models/User")
+const express = require("express");
+const router = new express.Router();
+const userModel = require("../models/User");
 
-router.post("/", (req,res) => {
-    userModel.create(req.body)
+router.post("/", (req, res) => {
+  userModel
+    .create(req.body)
     .then(dbRes => {
-        res.status(200).json(dbRes)
-    }).catch(dbErr => {
-        res.status(500).json(dbErr)
+      res.status(200).json(dbRes);
     })
+    .catch(dbErr => {
+      res.status(500).json(dbErr);
+    });
+});
+
+router.get("/myProfile/:id", (req, res) => {
+  userModel
+    .findById(req.params.id)
+    .then(dbRes => {
+        console.log(dbRes)
+      res.status(200).json(dbRes);
+    })
+    .catch(dbErr => {
+      res.status(200).json(dbErr);
+    });
+});
+
+router.post("/myProfile/edit/:id", (req, res) => {
+  userModel
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then(dbRes => {
+      res.status(200).json(dbRes);
+    })
+    .catch(dbErr => {
+      res.status(200).json(dbErr);
+    });
 });
 
 module.exports = router;
